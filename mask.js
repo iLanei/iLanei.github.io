dde312fe5516139c806abf131e45f1e5f1c1b850
Lain <lane1341@protonmail.com>
2020-09-30 14:55:34 +0200

    asdasd

--- mask.js
+++ mask.js
@@ -1,3 +1,6 @@
 function Cucc(){
     document.getElementById("szoveg").innerHTML = "Szosz";
+    var orig_ip = document.getElementById("orig_ip").value;
+
+    document.getElementById("szoveg").innerHTML = ip;
 }
