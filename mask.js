4253c43af8310de666fef67a88cce03b5caccab4
Lain <lane1341@protonmail.com>
2020-09-30 15:28:15 +0200

    khjd

--- mask.js
+++ mask.js
@@ -1,6 +1,7 @@
 function Cucc(){
     document.getElementById("szoveg").innerHTML = "Szosz";
     var orig_ip = document.getElementById("orig_ip").value;
+    var orig_mask = document.getElementById("orig_ip").value;
 
     document.getElementById("szoveg").innerHTML = ip;
 }
